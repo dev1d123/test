54a179eb3419fdb59e748aaa9e2803668aec6694
dev1d123 <dhuamanio@unsa.edu.pe>
2024-05-30 15:38:28 -0500

    Error, reorganizacion de la app Persona

--- src/personas/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
