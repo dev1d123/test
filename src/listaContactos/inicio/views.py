d716b9b3af3ee0e8f15949567ef1706984eaf80f
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-08 20:19:56 -0500

    (Diapositiva 10) -> Creando una nueva vista llamada anotherView y roteandola con el nombre another/

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra pagina</h1>')
