da76267c8ae9fd0fc2677af5be833f3258830ded
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-08 20:53:04 -0500

    (Diapositiva 12) -> Se crea la carpeta templates para almacenar los html, luego se enlaza la ruta de los enlaces y se llama al documento a traves de la vista

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def myHomeView(*args, **kwargs):
-    print(args)
-    print(kwargs)
-    return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+def myHomeView(request, *args, **kwargs):
+    return render(request, "home.html", {})
 
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
