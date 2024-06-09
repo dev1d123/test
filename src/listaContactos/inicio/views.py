3c7019be24ad8beaa4d5957020e5e3aaaa3e7d9d
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-08 20:22:26 -0500

    (Diapositiva 11) -> Prueba para ver que tipo te argumento reciben las vistas, reciben el nombre del usuario junto con una solicitud get

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
+    print(args)
+    print(kwargs)
     return HttpResponse('<h1>Hola Mundo desde Django</h1>')
 
 def anotherView(request):
