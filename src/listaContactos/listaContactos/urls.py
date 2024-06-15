c6fb596d766bc55e7fd7eadaa36507d20086a5ec
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-15 08:21:22 -0500

    (Diapositiva 19) -> Creacion de un form plano usando get para buscar ayuda en la pagina oficial de Django version 2.1

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -19,12 +19,13 @@ from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
 from personas.views import personaTestView
-from personas.views import personaCreateView
+from personas.views import personaCreateView, searchForHelp
 urlpatterns = [
     path('', myHomeView, name='Pagina de Inicio'),
     path('another/', anotherView),
     path('persona/', personaTestView, name='otro'),
     path('agregar/', personaCreateView, name='createPersona'),
+    path('search/', searchForHelp, name='buscar'),
 
     path('admin/', admin.site.urls),
     
