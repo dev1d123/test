a93dd52f05b05ecb8cb3dc988095d41f2c90c011
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-15 05:46:19 -0500

    Probando los metodos revcounter, first y last de forLoop

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -5,7 +5,9 @@ def myHomeView(request, *args, **kwargs):
     myContext = {
         'myText' : 'Esto es sobre nosotros',
         'myNumber': 123,
-        'myList' : {33,44,55},
+        'myList' : {33,44,55,66,77},
+        'myDict': {'clave1': 'valor1', 'clave2': 'valor2', 'clave3': 'valor3'},
+
     }
     return render(request, "home.html", myContext)
 
