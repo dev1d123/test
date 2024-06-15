1902ff97b045ae8d9aae8107d59398c868e01d36
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-15 07:40:56 -0500

    (Diapositiva 10) -> Renderizacion del modelos en la aplicacion persona, se crea el template y la vista, en la vista indicamos el contexto con los datos de la persona cuyo id=1

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
-
+from personas.views import personaTestView
 urlpatterns = [
     path('', myHomeView, name='Pagina de Inicio'),
-    path('another', anotherView),
+    path('another/', anotherView),
+    path('persona/', personaTestView, name='otro'),
+
     path('admin/', admin.site.urls),
+    
 ]
