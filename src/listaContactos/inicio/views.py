3a5ac362fa197958ac609301bbfadfd7f5e1bfe1
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-15 05:31:30 -0500

    (Diapositiva 3) -> Se agrega un contexto para enviar variables desde la vista, el contexto es un diccionario que incluye dos variables, myText y myNumber

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def myHomeView(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    myContext = {
+        'myText' : 'Esto es sobre nosotros',
+        'myNumber': 123,
+    }
+    return render(request, "home.html", myContext)
 
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
