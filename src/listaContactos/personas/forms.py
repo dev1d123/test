4c2052f3b124ffcbf167be4fc40b6d0cd627718f
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-23 19:41:35 -0500

    (Diapositiva 9) -> Se añaden algunos modificadores hooks a los campos, las opciones default de las ociones core es blank o null, en general depende del progama

--- src/listaContactos/personas/forms.py
+++ src/listaContactos/personas/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Persona
 
 class RawPersonaForm(forms.Form):
-    nombres = forms.CharField()
+    nombres = forms.CharField(label='Your Name')
     apellidos = forms.CharField()
-    edad = forms.IntegerField()
+    edad = forms.IntegerField(initial = 20)
     donador = forms.BooleanField()
