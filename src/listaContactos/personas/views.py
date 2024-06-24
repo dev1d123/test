07e148962a306bdbdd364349818c01641c617171
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-23 20:42:47 -0500

    (Diapositiva 15) -> Manejando el error 404 del servidor en caso de que se acceda a un objeto que no existe

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
 
@@ -32,7 +32,7 @@ def personaCreateView(request):
     return render(request, 'personas/personasCreate.html', context)
 
 def personasShowObject(request, myID):
-    obj = Persona.objects.get(id = myID)
+    obj = get_object_or_404(Persona, id=myID)
     context={
         'objeto': obj,
     }
