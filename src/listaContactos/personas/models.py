a88f4662f5e4c0d1396d74d71118ede5e9e43383
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-23 21:09:53 -0500

    (Diapositiva 20) -> Invirtiendo los URLs

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Persona(models.Model):
     nombres = models.CharField(max_length=100)
@@ -7,4 +7,4 @@ class Persona(models.Model):
     edad = models.IntegerField()#(max_digits=3)
     donador = models.BooleanField(blank=False) 
     def get_absolute_url(self):
-        return "/personas/" + str(self.id) + "/"
+        return reverse('browsing', kwargs={'myID': self.id})
