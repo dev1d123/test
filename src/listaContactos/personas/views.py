a5fe35eb1e7722c1d7f9637e579eca5d4dfacd4c
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-23 19:34:07 -0500

    (Diapositiva 6) -> Se agrega un validador para el form, si es correcto se imprimen en consola los datos enviados, al cambiar el dato, ocurre un error que se muestra en consola, django no admite ese determinado tipo de dato

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -6,7 +6,11 @@ from .forms import RawPersonaForm
 def personasAnotherCreateView(request):
     form = RawPersonaForm() #request.GET
     if request.method == "POST":
-       form = RawPersonaForm(request.POST)
+        form = RawPersonaForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
         'form': form,
     }
