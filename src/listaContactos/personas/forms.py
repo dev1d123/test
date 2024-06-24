80dcc0906c221a2a8042a3df0ca7c9dd83dcb679
dev1d123 <dhuamanio@unsa.edu.pe>
2024-06-23 19:45:20 -0500

    (Diapositiva 10) -> Se añade un widget para crear un Textarea con ciertos atributos para el campo nombres

--- src/listaContactos/personas/forms.py
+++ src/listaContactos/personas/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import Persona
 
 class RawPersonaForm(forms.Form):
-    nombres = forms.CharField(label='Your Name')
+    nombres = forms.CharField(
+        widget= forms.Textarea(
+            attrs={
+                'placeholder': 'Solo tu nombre porfavor',
+                'id': 'nombreID',
+                'class': 'special',
+                'cols:': '10',
+            }
+        )
+
+    )
     apellidos = forms.CharField()
-    edad = forms.IntegerField(initial = 20)
+    edad = forms.IntegerField()
     donador = forms.BooleanField()
